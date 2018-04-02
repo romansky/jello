6e7b7c762fb784750e51238891546f7901f1043a
romansky <roman.land@gmail.com>
2018-04-02 10:22:11 +0300

    touchups

--- jello/shared/src/main/scala/com/uniformlyrandom/jello/JelloValue.scala
+++ jello/shared/src/main/scala/com/uniformlyrandom/jello/JelloValue.scala
@@ -3,19 +3,18 @@ package com.uniformlyrandom.jello
 // trait extends Any so that implementing classes can be value classes
 // see http://docs.scala-lang.org/overviews/core/value-classes.html
 sealed trait JelloValue extends Any {
-  def name = getClass.getSimpleName.replace("$","")
+  def name: String = getClass.getSimpleName.replace("$", "")
 }
 
 object JelloValue {
-  case class JelloNumber(v: BigDecimal) extends AnyVal with JelloValue
-  case class JelloBool(v: Boolean) extends AnyVal with JelloValue
-  case class JelloString(v: String) extends AnyVal with JelloValue
+  case class JelloNumber(v: BigDecimal)                extends AnyVal with JelloValue
+  case class JelloBool(v: Boolean)                     extends AnyVal with JelloValue
+  case class JelloString(v: String)                    extends AnyVal with JelloValue
   case class JelloObject(map: Map[String, JelloValue]) extends JelloValue
-  case class JelloArray(seq: Seq[JelloValue]) extends JelloValue
-  case object JelloNull extends JelloValue
+  case class JelloArray(seq: Seq[JelloValue])          extends JelloValue
+  case object JelloNull                                extends JelloValue
 
   object JelloObject {
-    def apply(fields: Seq[(String,JelloValue)]): JelloObject = JelloObject(fields.toMap)
+    def apply(fields: Seq[(String, JelloValue)]): JelloObject = JelloObject(fields.toMap)
   }
 }
-
